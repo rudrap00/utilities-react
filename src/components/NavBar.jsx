19b8ca9093ada664b6d1c14c4da46c6fc9d2975c
Rudra Pratap <rudrap99.dba@gmail.com>
2024-04-03 11:23:16 +0530

    updated: minor changes in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,12 +4,16 @@ const NavBar = () => {
   return (
     <>
       <div className="w-full h-[3.5rem] bg-sky-500 flex items-center justify-center gap-10">
-        <div className="h-[2rem] w-[5rem] border-black border-[0.05rem] rounded-md flex items-center justify-center text-black hover:bg-sky-600 ">
-          <NavLink to="/utilities-react/">Home</NavLink>
-        </div>
-        <div className="h-[2rem] w-[11rem] border-black border-[0.05rem] rounded-md flex items-center justify-center text-black hover:bg-sky-600 ">
-          <NavLink to="currencyConverter">Currency Converter</NavLink>
-        </div>
+        <NavLink to="/utilities-react/">
+          <div className="h-[2rem] w-[5rem] border-black border-[0.05rem] rounded-md flex items-center justify-center text-black hover:bg-sky-600 ">
+            Home
+          </div>
+        </NavLink>
+        <NavLink to="currencyConverter">
+          <div className="h-[2rem] w-[11rem] border-black border-[0.05rem] rounded-md flex items-center justify-center text-black hover:bg-sky-600 ">
+            Currency Converter
+          </div>
+        </NavLink>
         <NavLink to="passwordGenerator">
           <div className="h-[2rem] w-[11rem] border-black border-[0.05rem] rounded-md flex items-center justify-center text-black hover:bg-sky-600 ">
             Password Generator
